8e8b5358ad1c654a0f039c6e57473bd19ee68705
Callum <callum@ischus.co.uk>
2019-04-13 10:35:23 +0100

    Add camera node to Player.tscn

--- April2018ConvenienceJam/Scripts/Player.gd
+++ April2018ConvenienceJam/Scripts/Player.gd
@@ -2,13 +2,15 @@ extends KinematicBody2D
 
 export var color : Color = Color(0, 0, 1)
 export var radius : float = 4.0
-export var speed : float = 1.0
+export var speed : float = 64.0
+
+var offset = 0.0
 
 func _process(delta):
 	var direction = Vector2(0.0, 0.0)
 	
 	if (Input.is_action_pressed("move_up")):
-		direction.x -= 1.0
+		direction.y -= 1.0
 	elif (Input.is_action_pressed("move_down")):
 		direction.y += 1.0
 	
@@ -17,8 +19,9 @@ func _process(delta):
 	elif (Input.is_action_pressed("move_right")):
 		direction.x += 1.0
 	
-	var collision = move_and_slide(direction * speed * delta)
+	# Move the player according to their direction
+	translate(move_and_slide(direction * speed * delta))
 
 func _draw():
 	# Draw a circle to represent the player character
-	draw_circle(position, radius, color)
+	draw_circle(position, radius + offset, color)
